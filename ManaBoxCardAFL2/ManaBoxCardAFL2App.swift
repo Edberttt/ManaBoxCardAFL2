6f75e9800baaac968fd990d466be48fbb95da546
Edbert <echandradinata@student.ciputra.ac.id>
2023-11-10 12:04:39 +0700

    Initial Commit

---
+++ ManaBoxCardAFL2/ManaBoxCardAFL2App.swift
@@ -0,0 +1,18 @@
+//
+//  ManaBoxCardAFL2App.swift
+//  ManaBoxCardAFL2
+//
+//  Created by MacBook Pro on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ManaBoxCardAFL2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
